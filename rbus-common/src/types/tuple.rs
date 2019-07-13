e8c16fbc0fd1b07a1e3449772596a01775716fc3
KokaKiwi <kokakiwi+git@kokakiwi.net>
2019-07-13 04:44:29 +0200

    rbus-common: Add some tests.

--- rbus-common/src/types/tuple.rs
+++ rbus-common/src/types/tuple.rs
@@ -22,3 +22,21 @@ impl_tuple_type!(0: A, 2: B, 3: C, 4: D, 5: E);
 impl_tuple_type!(0: A, 2: B, 3: C, 4: D, 5: E, 6: F);
 impl_tuple_type!(0: A, 2: B, 3: C, 4: D, 5: E, 6: F, 7: G);
 impl_tuple_type!(0: A, 2: B, 3: C, 4: D, 5: E, 6: F, 7: G, 8: H);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_signature_simple_packed1() {
+        assert_eq!(<(u64,)>::signature(), "t");
+    }
+
+    #[test]
+    fn test_signature_struct_packed() {
+        #[derive(DBusType)]
+        struct Arg(u8, String);
+
+        assert_eq!(<(u32, Arg)>::signature(), "u(ys)");
+    }
+}
